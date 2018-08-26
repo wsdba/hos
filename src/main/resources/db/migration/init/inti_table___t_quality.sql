63577bcf9cce3b79ed857d8ba0a9fac2aef03166
fang <izxyibeizi@vip.qq.com>
2018-08-26 16:09:49 +0800

    新增质控人员数据初始化脚本

---
+++ src/main/resources/db/migration/init/inti_table___t_quality.sql
@@ -0,0 +1,17 @@
+
+-- ----------------------------
+-- Records of t_quality
+-- ----------------------------
+INSERT INTO `t_quality` VALUES ('40284a816574772101657484c2f10001', '张三', '12', '0', '外科医生', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a816574e157016574e1ba060000', '刘备', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a816574e157016574e200ba0001', '关羽', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a816574e157016574e229d50002', '曹操', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a816574e157016574e267d00003', '典韦', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a0165751161720000', '赵子龙', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a01657511c7e80001', '孙权', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a01657511ed940002', '周瑜', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a0165751254cc0003', '诸葛亮', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a01657512ab490004', '诸葛瑾 ', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a0165751306fa0005', '关飞', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165750d2a0165751349330006', '李四', '12', '0', '外科', null, null, null, null, null);
+INSERT INTO `t_quality` VALUES ('40284a8165753c8e0165753d18390000', '大乔', '12', '0', '内科', null, null, null, null, null);
